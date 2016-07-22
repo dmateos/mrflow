88cb856c451b55272a99da2573cbebe015d6dfa5
Daniel Mateos <daniel@mateos.cc>
2016-07-23 06:16:42 +1000

    yet more test refactors

--- mrflow/spec/lib/simple_flow_handler_spec.rb
+++ mrflow/spec/lib/simple_flow_handler_spec.rb
@@ -4,20 +4,20 @@ describe MrFlow::SimpleFlowHandler do
   let(:fake_http) { double() }
   let(:host) { "http://localhost/flows" }
   let(:tag) { "tag" }
-  let(:pl) { { "payload" => "test-data" } }
+  let(:payload) { { "payload" => "test-data" } }
   subject { MrFlow::SimpleFlowHandler.new(host, fake_http) }
 
   describe "flow sending" do
     it "builds the correct url and sends the correct data to the flow" do
-      expect(fake_http).to receive("post").with("#{host}", anything).and_return(true)
-      expect(subject.send(tag, "test-data")).to eq(true)
+      expect(fake_http).to receive(:post).with("#{host}", anything).and_return(true)
+      expect(subject.send(tag, payload["payload"])).to eq(true)
     end
   end
 
   describe "flow receiving" do 
     it "builds the correct url and returns a flow based on a tag" do
-      expect(fake_http).to receive("get").with("#{host}/#{tag}", anything).and_return(pl)
-      expect(subject.receive(tag)).to eq(pl["payload"])
+      expect(fake_http).to receive(:get).with("#{host}/#{tag}", anything).and_return(payload)
+      expect(subject.receive(tag)).to eq(payload["payload"])
     end
   end
 end
